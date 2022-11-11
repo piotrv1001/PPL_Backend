ba1d165aa5f9989e2b06ab812a7c2a0c158a1456
piotrv-pc <piotrv1001@gmail.com>
2022-11-11 11:43:53 +0100

    user endpoints

---
+++ main.py
@@ -0,0 +1,111 @@
+import mysql.connector
+from flask import Flask
+from flask_restful import Api, Resource
+from flask import request
+from flask import abort
+# from flask import jsonify
+from user import User
+import jsonpickle
+
+def getConnection():
+    return mysql.connector.connect(
+        user = 'ppl-admin',
+        password = 'PPL-password-123',
+        host = '127.0.0.1',
+        database = 'ppl-project',
+        auth_plugin = 'mysql_native_password'
+    )
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/user', methods = ['GET'])
+def getUsers():
+    users = []
+    connection = getConnection()
+    cursor = connection.cursor(dictionary = True)
+    query = 'SELECT * FROM User;'
+    cursor.execute(query)
+
+    for row in cursor:
+        currentUser = User(
+            userId = row['userId'],
+            email = row['email'],
+            password = row['password'],
+            address = row['address'],
+            name = row['name'],
+            phoneNumber = row['phoneNumber'],
+            isAdmin = row['isAdmin']
+        )
+        users.append(currentUser)
+
+    connection.close()
+
+    return jsonpickle.encode(users, unpicklable = False)
+
+
+@app.route('/register', methods = ['POST'])
+def register():
+    requestData = request.get_json()
+    connection = getConnection()
+    cursor = connection.cursor()
+    query = 'INSERT INTO User(email, password, name, address, phoneNumber, isAdmin) VALUES (%(email)s, %(password)s, %(name)s, %(address)s, %(phoneNumber)s, %(isAdmin)s);'
+    cursor.execute(query, requestData)
+    connection.commit()
+    connection.close()
+
+    return requestData, 201
+
+
+@app.route('/login', methods = ['POST'])
+def login():
+    email = request.args.get('email', '')
+    password = request.args.get('password', '')
+    if email == '' or password == '':
+        # 400 - Bad Request
+        abort(400)
+    
+    connection = getConnection()
+    cursor = connection.cursor()
+    query = 'SELECT * FROM User WHERE email=%s AND password=%s;'
+    cursor.execute(query, (email, password))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        connection.close()
+        # 404 - Not Found
+        abort(404)
+
+    connection.close()
+    return ('', 200)
+
+
+@app.route('/user/<int:userId>', methods = ['GET'])
+def getUserById(userId):
+    connection = getConnection()
+    cursor = connection.cursor(dictionary = True)
+    query = "SELECT * FROM User WHERE userId=%s;"
+    cursor.execute(query, (userId,))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        connection.close()
+        # 404 - Not Found
+        abort(404)
+    
+    user = None
+    row = result[0]
+    user = User(
+        userId = row['userId'],
+        email = row['email'],
+        password = row['password'],
+        address = row['address'],
+        name = row['name'],
+        phoneNumber = row['phoneNumber'],
+        isAdmin = row['isAdmin']
+    )
+
+    return jsonpickle.encode(user, unpicklable = False)
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True, port = 5000)
---
+++ user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, userId, email, password, address, name, phoneNumber, isAdmin):
+        self.userId = userId
+        self.email = email
+        self.password = password
+        self.address = address
+        self.name = name
+        self.phoneNumber = phoneNumber
+        self.isAdmin = isAdmin
